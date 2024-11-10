3718963fbbda2a998ce43850b8f2056cbe99ffed
Ace007 <tarunkakani007@gmail.com>
2024-11-10 21:08:31 +0530

    JS File Added

---
+++ src/main.js
@@ -0,0 +1,4 @@
+var form = document.getElementById("loginForm");
+var emailInput = document.querySelector(".email")
+var passInput = document.querySelector(".password")
+// button is not needed as form can be reset through inbuilt func
